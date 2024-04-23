bb6585f004d90af4e4b4a665167f54b0ba2bf6ed
LoveAsAConstruct <isaiah.j.murray@gmail.com>
2024-04-23 18:53:36 -0400

    create 6 files and update 1 file

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,21 @@
 <script>
-	export let name;
+  import Header from './components/Header.svelte';
+  import Project from './components/Project.svelte';
+  import Footer from './components/Footer.svelte';
+
+  let projects = [
+    { title: 'Project One', description: 'A description of Project One.' },
+    { title: 'Project Two', description: 'A description of Project Two.' }
+  ];
 </script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main { padding: 20px; }
 </style>
+
+<Header />
+<main>
+  <h1>Welcome to My Portfolio</h1>
+  {#each projects as project}
+    <Project {project} />
+  {/each}
+</main>
---
+++ src/components/Project.svelte
@@ -0,0 +1,17 @@
+<!-- Project.svelte -->
+<script>
+  export let project;
+  let showModal = false;
+</script>
+
+<div on:click={() => showModal = true}>
+  {project.title}
+  <!-- more project details -->
+</div>
+
+{#if showModal}
+<div class="modal">
+  <!-- Modal content -->
+  <button on:click={() => showModal = false}>Close</button>
+</div>
+{/if}
