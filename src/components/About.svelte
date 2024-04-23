f585aa22a3c0c39c8d93133c6711b1720593171a
LoveAsAConstruct <isaiah.j.murray@gmail.com>
2024-04-23 19:16:13 -0400

    delete 21 files

--- src/components/About.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-  // Any JavaScript needed for the About page
-</script>
-
-<h1>About Us</h1>
-<p>This is the about page where you can put information about yourself or your company.</p>
--- src/components/Home.svelte
+++
@@ -1,2 +0,0 @@
-<h1>Welcome to My Portfolio</h1>
-<p>This is the homepage of your professional portfolio site.</p>
