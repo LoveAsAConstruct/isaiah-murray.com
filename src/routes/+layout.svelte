b05ff303eb6f81687b5606ea41ceba6ba8bca293
LoveAsAConstruct <isaiah.j.murray@gmail.com>
2024-05-28 18:56:57 -0400

    📝 (lib/footer.svelte): Add build date variable and social media links to footer 📝 (lib/header.svelte): Update navigation in header to include a 'Gallery' link 📝 (+layout.svelte): Import Header and Footer components, update styles and add date prop to Footer 📝 (static/styles/style.css): Refactor global resets and add specific styles for header and footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,23 @@
 <script>
-    console.log('Layout is being processed');
+  import Header from '$lib/header.svelte';
+  import Footer from '$lib/footer.svelte';
+  console.log('Layout is being processed');
 </script>
 
 <svelte:head>
-  <title>Svelte App</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="/favicon.png" type="image/png">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link rel="stylesheet" href="styles/style.css"> <!-- Assume global styles are renamed and correctly placed -->
-  <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500;700&display=swap" rel="stylesheet">
+<title>Svelte App</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="/favicon.png" type="image/png">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="/styles/style.css">
+<link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500;700&display=swap" rel="stylesheet">
 </svelte:head>
 
-<nav>
-<a href="/">About</a>
-<a href="/projects">Projects</a>
-<a href="/contact">Contact</a>
-<a href="/gallery">Gallery</a>
-</nav>
+<Header />
 
-<div style="margin: 30px;">
-    <slot></slot>  <!-- This slot tag is crucial -->
+<div style="margin: 60px 30px 30px 30px;">
+  <slot></slot>  <!-- This slot tag is crucial -->
 </div>
+
+<Footer buildDate="May 28, 2024" />
